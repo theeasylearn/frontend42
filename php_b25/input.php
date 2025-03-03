259fb42ddd693f2bce976d33c0173ed36fd2ab72
theeasylearn <theeasylearn@gmail.com>
2025-03-03 19:34:43 +0530

    error handling

--- php_b25/input.php
+++ php_b25/input.php
@@ -6,7 +6,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body oncontextmenu="return false;">
+<body oncontextmenu="return false;"><!-- insepact disble to use this code -->
     <div class="container">
         <div class="row">
             <div class="col-12">
--- php_b25/submit/loop.php
+++ php_b25/submit/loop.php
@@ -20,6 +20,39 @@
             }
             echo "</table>";
             break;
+
+        case 2:
+            echo "<table border='1' align='center' bgcolor='lightgreen'>";
+            for($i=1;$i<=$row;$i++)
+            {
+                echo "<tr>";
+                for($j=1;$j<=$col;$j++)
+                {
+                    echo "<td>Shubham</td>";
+                }
+                echo "</tr>";
+            }
+            echo "</table>";
+            break;
+
+        case 3:
+            echo "<table border='1' align='center' bgcolor='lightyellow'>";
+            $i=1;
+            do
+            {
+                 echo "<tr>";
+                 $j=1;
+                 do
+                 {
+                    echo "<td>Rita</td>";
+                    $j++;
+                 }while($j<=$col);
+                 echo "</tr>";
+                 $i++;
+            }while($i<=$row);
+            echo "</table>";
+            break;
+        
   }
 
 
