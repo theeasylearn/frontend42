20bf39b2323567b5faf981834c020e935dcbd81c
theeasylearn <theeasylearn@gmail.com>
2025-03-05 19:37:53 +0530

    try_catch

--- php_b25/try_catch_finally1.php
+++ php_b25/try_catch_finally1.php
@@ -1,13 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Answer</title>
+</head>
+<body>
+    <h1>Answer :</h1>
+</body>
+</html>
 <?php
-
+     date_default_timezone_set("Asia/Kolkata");
    try
    {
         $no1 = 100;
-        $no2 = 50;
+        $no2 = 0;
 
        if($no2 == 0)
        {
-           throw new exception("This number is not dividable");
+           throw new exception("This number is not dividable\n");
        }
        $result = $no1 / $no2;
        echo "result is  : ".$result;
@@ -18,12 +29,26 @@
    }
    catch (exception $e)
    {
-        echo "Error :".$e->getMessage();
-        echo "<br>";
+       // echo "Error :".$e->getMessage();
+       // echo "<br>";
+
+       $lfile = "error.log";//crete file name 
+      
+       $datetime = date("d-m-Y  h:i:s A");// date and time for error message
+       $error = "\n[{$datetime}] error is generated {$file}"."\n".$e->getMessage();
+
+       $file=fopen($lfile,"a");//open file append the error in file 
+       fprintf($file,$error);//write message in file using fprintf
+       fclose($file);//then close file for using fclose
+
+       header("location:error.php");// redrict page when error is coming in program
+
+       exit();//for when file is complete work then exit from file
+
    }
-   finally
+  /* finally
    {
        echo "this opertion run succefully";
-   }
+   }*/
 
 ?>
