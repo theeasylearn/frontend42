923ca497a911a8dc396298e8a1c3049df405f021
theeasylearn <theeasylearn@gmail.com>
2025-03-29 19:32:21 +0530

    insert_php

---
+++ php_b25/project/inc/insert.php
@@ -0,0 +1,21 @@
+<?php
+    require_once("connection.php");
+    
+        $qry = $db->prepare("INSERT INTO user (name, email, age) VALUES (:name, :email, :age)");
+
+        // Bind parameters
+        $qry->bindParam(':name', $name);
+        $qry->bindParam(':email', $email);
+        $qry->bindParam(':age', $age);
+
+        // Sample data
+        $name = 'Ayushiba';
+        $email = 'ayu@gmail.com';
+        $age = 21;
+
+        // Execute the query
+        $qry->execute();
+
+        echo "Your record is inserted....";
+
+?>
